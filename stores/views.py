aa4d7bad463b1952a3480e75f7b5d2f3aa2c654a
Rafael Alberto Larios <albertolarios@me.com>
2023-02-05 06:27:41 -0600

    Add view ListAPIView

--- stores/views.py
+++ stores/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Pizzeria
+from .serializers import PizzeriaListSerializer
+
 
 # Create your views here.
+class PizzeriaListAPIView(generics.ListAPIView):
+    queryset = Pizzeria.objects.all()
+    serializer_class = PizzeriaListSerializer
