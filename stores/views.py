bd534f0ce805852d36b90fe92bff4b8a73a51a84
Rafael Alberto Larios <albertolarios@me.com>
2023-02-05 13:00:48 -0600

    Add retrieve or detail api view

--- stores/views.py
+++ stores/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from rest_framework import generics
 from .models import Pizzeria
-from .serializers import PizzeriaListSerializer
+from .serializers import PizzeriaListSerializer, PizzeriaDetailSerializer
 
 
 # Create your views here.
 class PizzeriaListAPIView(generics.ListAPIView):
     queryset = Pizzeria.objects.all()
     serializer_class = PizzeriaListSerializer
+
+
+class PizzeriaRetrieveAPIView(generics.RetrieveAPIView):
+    lookup_field = "id"
+    queryset = Pizzeria.objects.all()
+    serializer_class = PizzeriaDetailSerializer
