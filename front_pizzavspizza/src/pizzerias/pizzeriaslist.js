5d9de3f8a6455ac41d0f02fa26d88561ad000c99
Rafael Alberto Larios <albertolarios@me.com>
2023-02-06 17:53:44 -0600

    Show pizzeria details on list click

--- front_pizzavspizza/src/pizzerias/pizzeriaslist.js
+++ front_pizzavspizza/src/pizzerias/pizzeriaslist.js
@@ -8,11 +8,32 @@ class PizzaList extends React.Component {
         super(props);
         this.state = {
             pizzerias: [],
+            pizzeria: null,
+            showComponent: false,
         };
+
+        this.getPizzeriaDetails = this.getPizzeriaDetails.bind(this);
+        this.showPizzeriaDetails = this.showPizzeriaDetails.bind(this);
+    }
+
+    getPizzeriaDetails(pizzeria) {
+        const response = axios
+            .get('http://127.0.0.1:8000'.concat(pizzeria.absolute_url))
+            .then((response) => {
+                this.setState({ pizzeria: response.data });
+            })
+            .catch((err) => console.log(err));
+        return response;
+    }
+
+    showPizzeriaDetails(pizzeria) {
+        this.getPizzeriaDetails(pizzeria)
+            .then((response) => this.setState({ showComponent: true }));
     }
 
     componentDidMount() {
-        axios.get('http://localhost:8000/')
+        axios
+            .get('http://127.0.0.1:8000/')
             .then((response) => {
                 console.log('Is this still running twice?')
                 this.setState({ pizzerias: response.data });
@@ -31,10 +52,15 @@ class PizzaList extends React.Component {
                 {this.state.pizzerias.map((pizzeria) => (
                     <li
                         className='pizzeria-entry'
-                        key={pizzeria.id} >
+                        key={pizzeria.id}
+                        onClick={() => this.showPizzeriaDetails(pizzeria)}
+                    >
                         {pizzeria.name} - {pizzeria.city}
                     </li>
                 ))}
+                {this.state.showComponent ? (
+                    <PizzaDetail pizzeria={this.state.pizzeria} />
+                ) : null}
             </ul>
         );
     }
