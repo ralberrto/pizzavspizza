ae5a4c5f99825f7d500bb9f92d474f0e34d7e8e0
Rafael Alberto Larios <albertolarios@me.com>
2023-02-06 12:59:57 -0600

    Replace DummyData with data recieved from the API call

--- front_pizzavspizza/src/pizzerias/pizzeriaslist.js
+++ front_pizzavspizza/src/pizzerias/pizzeriaslist.js
@@ -1,13 +1,34 @@
 import React from 'react';
-import DummyData from './dummydata.json';
 import PizzaDetail from './pizzeriadetails';
+import axios from 'axios';
 
 
 class PizzaList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            pizzerias: [],
+        };
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:8000/')
+            .then((response) => {
+                console.log('Is this still running twice?')
+                this.setState({ pizzerias: response.data });
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+        //fetch('http://localhost:8000', { mode: 'cors' })
+        //    .then((response) => response.json())
+        //    .then((response) => console.log(response));
+    }
+
     render() {
         return (
             <div className='pizzeria-list'>
-                {DummyData.map((pizzeria) => (
+                {this.state.pizzerias.map((pizzeria) => (
                     <PizzaDetail key={pizzeria.id} pizzeria={pizzeria} />
                 ))}
             </div>
