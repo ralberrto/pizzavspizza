491ec8ddc3a801beb698f133bceb1bacf04ef1e4
Rafael Alberto Larios <albertolarios@me.com>
2023-02-05 20:57:21 -0600

    Set initial frontend state

---
+++ front_pizzavspizza/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Web App for Pizza Lovers
+        </p>
+        <h1>
+          Pizza vs Pizza
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
