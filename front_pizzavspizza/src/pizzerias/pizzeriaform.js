aa90ef61abb7974f622280c0f76e91fdd3985297
Rafael Alberto Larios <albertolarios@me.com>
2023-02-06 19:16:23 -0600

    Add remaining input fields and handle submit with post method to API

--- front_pizzavspizza/src/pizzerias/pizzeriaform.js
+++ front_pizzavspizza/src/pizzerias/pizzeriaform.js
@@ -7,6 +7,14 @@ class PizzaForm extends React.Component {
         super(props);
         this.state = {
             name: '',
+            street: '',
+            city: '',
+            state: '',
+            zip_code: '',
+            website: '',
+            phone_number: '',
+            description: '',
+            email: '',
         };
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -19,17 +27,92 @@ class PizzaForm extends React.Component {
     }
 
     handleSubmit(event) {
+        event.preventDefault();
+        axios
+            .post('http://127.0.0.1:8000/create/', {
+                name: this.state.name,
+                street: this.state.street,
+                city: this.state.city,
+                state: this.state.state,
+                zip_code: this.state.zip_code,
+                website: this.state.website,
+                phone_number: this.state.phone_number,
+                description: this.state.description,
+                email: this.state.email,
+            })
+            .then((response) => console.log(response))
+            .catch((err) => console.log(err));
     }
 
     render() {
-        const { name } = this.state;
+        const {
+            name,
+            street,
+            city,
+            state,
+            zip_code,
+            website,
+            phone_number,
+            description,
+            email,
+        } = this.state;
         return (
             <form onSubmit={this.handleSubmit}>
                 <div>
+                    Pizzeria
                     <input
                         type='text'
                         name='name'
                         value={name}
+                        onChange={this.handleChange}
+                    />
+                    Address
+                    <input
+                        type='text'
+                        name='street'
+                        value={street}
+                        onChange={this.handleChange}
+                    />
+                    City
+                    <input
+                        type='text'
+                        name='city'
+                        value={city}
+                        onChange={this.handleChange}
+                    />
+                    State
+                    <input
+                        type='text'
+                        name='state'
+                        value={state}
+                        onChange={this.handleChange}
+                    />
+                    Zip
+                    <input
+                        type='text'
+                        name='zip_code'
+                        value={zip_code}
+                        onChange={this.handleChange}
+                    />
+                    Website
+                    <input
+                        type='url'
+                        name='website'
+                        value={website}
+                        onChange={this.handleChange}
+                    />
+                    Phone
+                    <input
+                        type='tel'
+                        name='phone_number'
+                        value={phone_number}
+                        onChange={this.handleChange}
+                    />
+                    Description
+                    <input
+                        type='textarea'
+                        name='description'
+                        value={description}
                         onChange={this.handleChange}
                     />
                 </div>
